3e688c5ba4c19b0e6f334ce9ecc70b20e2ebb79d
Vadim834 <74491947+Vadim834@users.noreply.github.com>
2021-04-05 08:07:38 +0300

    Add files via upload

---
+++ Src/lab17/main.cpp
@@ -0,0 +1,16 @@
+
+#include "Test.h"
+
+int main() {
+	testSort();
+	int size = 3;
+	struct Message* messages;
+	messages = (struct Message*)malloc(size * sizeof(struct Message));
+	fread(messages,size);
+	write(messages,size);
+	sort(messages,size);
+	fwrite(messages,size);
+	
+	
+	return 0;
+}
